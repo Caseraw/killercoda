26c9d6d0078334ec5f7bd4d8ff61a4443549a42e
Kasra <kasra.amirsarvari@gmail.com>
2023-10-30 06:35:34 +0100

    fix

--- games/color-race-game/setup.sh
+++ games/color-race-game/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 kubectl apply -f /root/setup.yaml
+
+kubectl -n color-race-game wait pod -l app=color-app --for=condition=Ready
+
+kubectl -n color-race-game port-forward services/color-app-service 80:80 --address 0.0.0.0
